5e0b4ad23949603d673ce45795110ee1cb553b66
Sergey Postnikov <ser.post2017@gmail.com>
2023-02-15 21:25:59 +0300

    add inline 'owner' to FlatAdmin

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Flat, Complain, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ('owner',)
+
+
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner', 'new_building')
     readonly_fields = ('created_at',)
@@ -15,13 +21,16 @@ class FlatAdmin(admin.ModelAdmin):
         )
     list_editable = ('new_building',)
     raw_id_fields = ('liked_by',)
+    inlines = [OwnerInline,]
 
 
+@admin.register(Complain)
 class ComplainAdmin(admin.ModelAdmin):
     fields = ('user', 'flat', 'text')
     raw_id_fields = ('user', 'flat')
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
     list_display = (
@@ -30,8 +39,3 @@ class OwnerAdmin(admin.ModelAdmin):
         'owners_phonenumber'
         )
 
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complain, ComplainAdmin)
-admin.site.register(Owner, OwnerAdmin)
-    
