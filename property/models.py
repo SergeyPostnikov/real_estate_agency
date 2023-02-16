2d59cf485a33f2c9b6cc29edeb8242c762931e53
Sergey Postnikov <ser.post2017@gmail.com>
2023-02-16 17:09:11 +0300

    specified related name

--- property/models.py
+++ property/models.py
@@ -48,7 +48,10 @@ class Flat(models.Model):
         db_index=True)
 
     new_building = models.BooleanField('Новостройка', default=False)
-    liked_by = models.ManyToManyField(User,  related_name='flats', verbose_name="Кто лайкнул")
+    liked_by = models.ManyToManyField(
+                                      User,  
+                                      related_name='liked_flats', 
+                                      verbose_name="Кто лайкнул")
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
